2354662b5e511abd83faee590710983ff2c10d58
Kevin Easterbrook <kevineasterbrook@gmail.com>
2021-02-11 23:27:24 +0200

    Some C#9 Features

---
+++ ValueObjects.Application/Services/Person_C9Service.cs
@@ -0,0 +1,13 @@
+﻿using ValueObjects.Domain.Aggregates.Person_C9;
+
+namespace ValueObjects.Application.Services
+{
+    public class Person_C9Service
+    {
+        public void CreatePerson()
+        {
+            var address = new AddressC9("4", "Table Mountain", "Cape Town", "7441", "Western Cape", "South Africa");
+            var otherAddress = address with { City = "Other City" };
+        }
+    }
+}
---
+++ ValueObjects.Domain/Aggregates/Person_C9/AddressC9.cs
@@ -0,0 +1,11 @@
+﻿namespace ValueObjects.Domain.Aggregates.Person_C9
+{
+    public record AddressC9(string StreetNumber, string Street, string City, string PostalCode, string Province, string Country)
+    {
+
+        public void Post()
+        {
+            // Some Post Logic
+        }
+    }
+}
